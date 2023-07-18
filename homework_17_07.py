8695f8e12666541d70fa48f0910899e948b7d322
GiorgioFranco715 <138186314+GiorgioFranco715@users.noreply.github.com>
2023-07-18 12:12:01 +0300

    homework_17_07

---
+++ homework_17_07.py
@@ -0,0 +1,79 @@
+if __name__ == "__main__":
+    #print("zdorov")
+
+    #Завдання 1
+    #Користувачористувач вводить із клавіатури номер дня тижня (1-7).
+    # Необхідно вивести на екран назву дня тижня.
+    # Наприклад, якщо 1, то на екрані напис "понеділок", 2 — "вівторок" і т.д.
+
+    #Дані від користувача:
+    # number_of_day = input("Введіть порядковий номер дня тижня від 1 до 7: ")
+
+    #Розв'язок задачі
+    # if number_of_day =="1":
+    #     print("Понеділок")
+    # elif number_of_day =="2":
+    #     print("Вівторок")
+    # elif number_of_day =="3":
+    #     print("Середа")
+    # elif number_of_day =="4":
+    #     print("Четвер")
+    # elif number_of_day =="5":
+    #     print("П'ятниця")
+    # elif number_of_day =="6":
+    #     print("Субота")
+    # elif number_of_day =="7":
+    #     print("Неділя")
+    # else:
+    #     print("Ви введи некоректні дані")
+
+    #Завдання 2
+    #Користувач вводить із клавіатури номер місяця (1-12).
+    # Необхідно вивести на екран назву місяця.
+    # Наприклад, якщо 1, то на екрані напис "січень", 2 — "лютий" і т.д.
+
+    # Дані від користувача:
+
+     # number_of_mounth = input('Введіть порядковий номер дня тижня від 1 до 12:')
+
+     # Розв'язок задачі
+
+     # if number_of_mounth =='1':
+     #        print("Січень")
+     # elif number_of_mounth =='2':
+     #        print('Лютий')
+     # elif number_of_mounth =='3':
+     #        print('Березень')
+     # elif number_of_mounth =='4':
+     #        print('Квітень')
+     # elif number_of_mounth =='5':
+     #        print('Травень')
+     # elif number_of_mounth =='6':
+     #        print('Червень')
+     # elif number_of_mounth =='7':
+     #        print('Липень')
+     # elif number_of_mounth =='8':
+     #        print('Серпень')
+     # elif number_of_mounth =='9':
+     #        print('Вересень')
+     # elif number_of_mounth =='10':
+     #        print('Жовтень')
+     # elif number_of_mounth =='11':
+     #        print('Листопад')
+     # elif number_of_mounth =='12':
+     #        print('Грудень')
+     # else:
+     #     print('Ви ввели некоректні дані')
+
+
+    #Завдання 3
+    #Користувач вводить із клавіатури число.
+    # Якщо число більше нуля, потрібно вивести напис "Number is positive",
+    # якщо менше нуля — "Number is negative", якщо дорівнює нулю — "Number is equal to zero".
+
+
+
+
+
+
+
